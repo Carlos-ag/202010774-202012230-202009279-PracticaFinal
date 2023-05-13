5d7bc02ece2c98e73dad2a607b7650b444989336
Carlos-ag <carlosmartindeargila@outlook.es>
2023-05-13 22:48:31 +0200

    changes to login and register

--- Stockify/javascript/register.js
+++ Stockify/javascript/register.js
@@ -1,26 +1,60 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('http://localhost:8080/subscriptionPlans')
-    .then(response => response.json())
-    .then(data => {
-        let select = document.getElementById('subscription');
-        data.forEach((item) => {
-            let option = document.createElement('option');
-            option.value = item.id;
-            option.text = item.name + ' ($' + item.price + ')';
-            select.appendChild(option);
-        });
-    });
+// Get all the input elements
+const inputs = document.querySelectorAll('input, textarea, select');
 
-    document.getElementById('register-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        let form = event.target;
-        let user = {
-            name: form.name.value,
-            email: form.email.value,
-            phone: form.phone.value,
-            subscriptionPlanId: form.subscription.value,
-            password: form.password.value
-        };
-        console.log(user);
+// Iterate over each input element
+inputs.forEach((input) => {
+    // Add event listener for 'input' event
+    input.addEventListener('input', (event) => {
+        validateInput(event.target);
     });
 });
+
+// Add event listener for 'submit' event
+document.getElementById('register-form').addEventListener('submit', (event) => {
+    // Prevent default form submission
+    event.preventDefault();
+
+    // Get all inputs excluding the login button
+    const inputs = document.querySelectorAll('input:not(#login-button), textarea, select');
+    
+    // Validate all input fields on form submission
+    inputs.forEach(validateInput);
+});
+
+
+// Function to validate an input field
+function validateInput(input) {
+    if (!input.value) {
+        input.setCustomValidity('This field is required.');
+        input.reportValidity();
+    } else {
+        input.setCustomValidity('');
+    }
+}
+
+let subscriptionSelect = document.getElementById('subscription');
+
+fetch('http://localhost:8080/subscriptionPlans')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.length > 0) {
+            subscriptionSelect.required = true;
+            data.forEach((item) => {
+                let option = document.createElement('option');
+                option.value = item.id;
+                option.text = item.name + ' ($' + item.price + ')';
+                subscriptionSelect.appendChild(option);
+            });
+        } else {
+            subscriptionSelect.required = false;
+        }
+    })
+    .catch(error => {
+        subscriptionSelect.required = false;
+        // alert('Hubo un problema con la solicitud: ' + error.message);
+    });

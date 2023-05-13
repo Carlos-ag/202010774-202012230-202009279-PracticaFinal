346a1f77a00f859f110b5bf3e7265ffa330e3145
CarlosMari <97597629+CarlosMari@users.noreply.github.com>
2023-05-13 11:48:32 +0200

    Added password encryption functions and testing

---
+++ stockify-api/src/test/java/com/stockify/stockifyapp/common/PasswordEncrypterTest.java
@@ -0,0 +1,27 @@
+package com.stockify.stockifyapp.common;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PasswordEncrypterTest{
+    private String pwd;
+    private PasswordEncrypter pe;
+    private String enPwd;
+    
+
+    @Test
+    public void testEncrypt() throws Exception {
+        pwd = "TestString";
+        pe = new PasswordEncrypter();
+        enPwd = pe.encrypt(pwd);
+        assertNotEquals(pwd,enPwd);
+    }
+    @Test
+    public void testDecrypt() throws Exception {
+        pwd = "TestString";
+        pe = new PasswordEncrypter();
+        assertEquals(pwd,pe.decrypt(pe.encrypt(pwd)));
+    }
+}
